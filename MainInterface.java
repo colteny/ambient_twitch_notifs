c0af98496f5ab71675526f2d901200be615e677d
leonzl <leonz@hawaii.edu>
2015-12-04 12:52:56 -1000

    File reading working, adjusted button size, settings message box
    The directories of the settings file and sound file may need changing
    depending on how your directory is like

--- MainInterface.java
+++ MainInterface.java
@@ -19,104 +19,160 @@ import org.json.JSONObject;
 
 import java.text.NumberFormat;
 import javax.swing.text.NumberFormatter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 /*
  * Credit for checkOnline & readFromURL method code: https://stackoverflow.com/questions/21926374/checking-if-a-twitch-tv-stream-is-online-and-receive-viewer-counts-using-pircbot
  */
 
 public class MainInterface {
-	
+
 	private static ArrayList<String> channels = new ArrayList<String>();
 	private static ArrayList<JButton> buttons = new ArrayList<JButton>();
 	private static ArrayList<Integer> alreadyOnline = new ArrayList<Integer>();
 	private static Runtime rt = Runtime.getRuntime();
 	private static int updateTime = 10;
-	
+	private static String currentDirectory = new File("").getAbsolutePath();
 
-	
+
+
 	public static void main(String[] argv) throws IOException, JSONException {
-		
-		//Initialize channels
-		channels.add("Jankos");
-		channels.add("C9Sneaky");
-		channels.add("flosd");
-		channels.add("Trick2g");
-		
-	  Color startColor = new Color(75,225,225);
-	  Color endColor = new Color(225,75,75);
 
-		
+		String  thisLine = null;
+		try{
+			// initial settings
+			System.out.println(currentDirectory);
+			BufferedReader br = new BufferedReader(new FileReader(new File(currentDirectory + "\\src\\ambient_twitch_notifs\\settings.txt")));
+			thisLine = br.readLine();
+			if(!isInteger(thisLine) || thisLine == null) {
+				updateTime = 10;
+				//if rest of file is empty
+				if((thisLine = br.readLine()) == null) {
+					br.close();
+					System.out.println("closed");
+					return;
+				}
+			}
+			else {
+				updateTime = Integer.parseInt(thisLine);
+				thisLine = br.readLine();
+
+			}
+			System.out.println(updateTime);
+			while (thisLine != null) {
+				//need code to check if users exist
+				channels.add(thisLine);
+				System.out.println(thisLine);
+				thisLine = br.readLine();
+			}
+
+			br.close();
+			System.out.println("closed");
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		//Initialize channels
+		/**
+		 channels.add("Jankos");
+		 channels.add("C9Sneaky");
+		 channels.add("flosd");
+		 channels.add("Trick2g");**/
+
+		Color startColor = new Color(75,225,225);
+		Color endColor = new Color(225,75,75);
+
+
 		//create and initialize frames and menus
 		//Frame
 		JFrame frame = new JFrame("Twitch Ambient Interface");
 		frame.setLayout(new GridLayout(0, 3));
 		JButton menu = new JButton("Settings");
-		menu.addActionListener(new ActionListener()
-			{				
-				public void actionPerformed(ActionEvent e) {
-					JFrame frame = new JFrame("Settings");
-					JPanel list = new JPanel();
-					
-					list.setLayout(new BoxLayout(list, BoxLayout.Y_AXIS));
+		menu.addActionListener(new ActionListener() {
+								   public void actionPerformed(ActionEvent e) {
+									   JFrame frame = new JFrame("Settings");
+									   JPanel list = new JPanel();
 
-					NumberFormat intFormat = NumberFormat.getIntegerInstance();
-					NumberFormatter numberFormatter = new NumberFormatter(intFormat);
-					numberFormatter.setValueClass(Integer.class); //ensures integer value
-					numberFormatter.setAllowsInvalid(false); //never allows anything else
-					numberFormatter.setMinimum(0); //lowest value
-					numberFormatter.setMaximum(Integer.MAX_VALUE); //max value of an integer, change it if you like
+									   list.setLayout(new BoxLayout(list, BoxLayout.Y_AXIS));
 
-          JLabel ref = new JLabel("Refresh Time Cycle:  (in seconds)");	//updateTime local variable, asking for 'final'
-          JTextField refreshTime = new JFormattedTextField(numberFormatter);;
-          JButton changeTime = new JButton("Accept");
-          changeTime.addActionListener(new ActionListener()
-          		{
-        	  		public void actionPerformed(ActionEvent e) {
-						updateTime = Integer.parseInt(refreshTime.getText());
-						System.out.println(updateTime);
-					}
-        		});
-					list.add(ref);
-					list.add(refreshTime);
-					list.add(changeTime);
-					list.add(Box.createRigidArea(new Dimension(0,7)));
-					
-					JLabel sn = new JLabel("Add new Streamer");
-          JTextField streamer = new JTextField(15);
-          JButton addStream = new JButton("Add");
-          addStream.addActionListener(new ActionListener()
-        	 {
-        	  	  public void actionPerformed(ActionEvent e) {
-        	  	  	try {
-        				if(getResponseCode("http://www.twitch.tv/" + streamer.getText()) != 404) {
-        	  	  			channels.add(streamer.getText());
-        	  	  		}
-        	  	  	} catch (IOException ioe) {
-        	  	  		ioe.printStackTrace();
-        	  	  	}
-        	  	      		
-        	  	  }
-        	}
-          );
-          list.add(sn);
-					list.add(streamer);
-					list.add(addStream);
-					
-					frame.add(list);
-					frame.pack();
-					frame.setVisible(true);
-				}			
-			}
+									   NumberFormat intFormat = NumberFormat.getIntegerInstance();
+									   NumberFormatter numberFormatter = new NumberFormatter(intFormat);
+									   numberFormatter.setValueClass(Integer.class); //ensures integer value
+									   numberFormatter.setAllowsInvalid(false); //never allows anything else
+									   numberFormatter.setMinimum(0); //lowest value
+									   numberFormatter.setMaximum(Integer.MAX_VALUE); //max value of an integer, change it if you like
+
+									   JLabel ref = new JLabel("Refresh Time Cycle:  (in seconds)");
+									   JTextField refreshTime = new JFormattedTextField(numberFormatter);
+									   JButton changeTime = new JButton("Accept");
+									   changeTime.addActionListener(new ActionListener() {
+										   public void actionPerformed(ActionEvent e) {
+											   updateTime = Integer.parseInt(refreshTime.getText());
+											   System.out.println(updateTime);
+										   }
+									   });
+									   list.add(ref);
+									   list.add(refreshTime);
+									   list.add(changeTime);
+									   list.add(Box.createRigidArea(new Dimension(0, 7)));
+
+									   JLabel sn = new JLabel("Add new Streamer");
+									   JTextField streamer = new JTextField(15);
+									   JButton addStream = new JButton("Add");
+									   addStream.addActionListener(new ActionListener() {
+																	   public void actionPerformed(ActionEvent e) {
+																		   try {
+																			   if (getResponseCode("http://www.twitch.tv/" + streamer.getText()) != 404) {
+																				   channels.add(streamer.getText());
+																			   }
+																		   }
+																		   catch (IOException ioe) {
+																			   ioe.printStackTrace();
+																		   }
+
+																	   }
+																   }
+									   );
+
+									   list.add(sn);
+									   list.add(streamer);
+									   list.add(addStream);
+
+
+									   JLabel ms = new JLabel("Messages");
+									   //messages if anything goes wrong in adding someone
+									   final JTextArea messages = new JTextArea(1, 20);
+									   messages.setEditable(false);
+
+									   list.add(ms);
+									   list.add(messages);
+
+									   frame.add(list);
+									   frame.pack();
+									   frame.setVisible(true);
+								   }
+							   }
 		);
+
 		menu.setPreferredSize(new Dimension(85, 85));
-    buttons.add(menu);
-		
-	  // Add a window listener for close button
+		buttons.add(menu);
+
+		// Add a window listener for close button
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-	  });
-		
+		});
+
 		//Populate Grid by creating buttons
 		for(int i=0; i<channels.size(); i++) {
 			final String channelName = channels.get(i);
@@ -124,30 +180,30 @@ public class MainInterface {
 			//testing to see if color change and sound works.
 			//alreadyOnline.add(0);
 			b1.addActionListener(new ActionListener()
-      {
-      	public void actionPerformed(ActionEvent e)
-      	{
-      		String url = "http://www.twitch.tv/" + channelName;
-      		String os = System.getProperty("os.name").toLowerCase();
-      		
-      		if (os.indexOf( "win" ) >= 0) {
-      		  try {
+			{
+				public void actionPerformed(ActionEvent e)
+				{
+					String url = "http://www.twitch.tv/" + channelName;
+					String os = System.getProperty("os.name").toLowerCase();
+
+					if (os.indexOf( "win" ) >= 0) {
+						try {
 							rt.exec( "rundll32 url.dll,FileProtocolHandler " + url);
 						} catch (IOException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-      		}
-      		else if(os.indexOf( "mac" ) >= 0) {
-      			try {
+					}
+					else if(os.indexOf( "mac" ) >= 0) {
+						try {
 							rt.exec( "open " + url);
 						} catch (IOException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
-      		}
-      	}
-      });
+					}
+				}
+			});
 			b1.setToolTipText(channels.get(i));
 			if(checkIfOnline(channels.get(i))) {
 				alreadyOnline.add(1);
@@ -157,8 +213,8 @@ public class MainInterface {
 				alreadyOnline.add(0);
 				b1.setBackground(endColor);
 			}
-      b1.setPreferredSize(new Dimension(85, 85));
-      buttons.add(b1);
+			b1.setPreferredSize(new Dimension(125, 50));
+			buttons.add(b1);
 		}
 		//add buttons
 		for(int i = 0;i < buttons.size();i++) {
@@ -167,14 +223,14 @@ public class MainInterface {
 		//create frame
 		frame.pack();
 		frame.setVisible(true);
-		
+
 		//update button colors
 		while(true) {
 			for(int i=0; i<channels.size(); i++) {
 				if(checkIfOnline(channels.get(i))) {
 					//checks if the person is already set to Online
 					if(alreadyOnline.get(i)!=1) {
-					//Plays sound when someone comes online
+						//Plays sound when someone comes online
 						alreadyOnline.set(i, 1);
 						try {
 							playAudio();
@@ -193,8 +249,8 @@ public class MainInterface {
 								e1.printStackTrace();
 							}
 						}
-					  startColor = new Color(75,225,225);
-					  endColor = new Color(225,75,75);
+						startColor = new Color(75,225,225);
+						endColor = new Color(225,75,75);
 					}
 				}
 				else {
@@ -212,75 +268,85 @@ public class MainInterface {
 								e1.printStackTrace();
 							}
 						}
-					  startColor = new Color(75,225,225);
-					  endColor = new Color(225,75,75);
+						startColor = new Color(75,225,225);
+						endColor = new Color(225,75,75);
 					}
 				}
 			}
 			//Delay before next refresh
-		  try {
-		    Thread.sleep(updateTime * 1000);                 //1000 milliseconds is one second.
-		  } 
-		  catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		  }
+			try {
+				Thread.sleep(updateTime * 1000);                 //1000 milliseconds is one second.
+			}
+			catch(InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
 		}
 
-}
+	}
 
-//Checks if a Stream is online
-public static boolean checkIfOnline(String channel) throws IOException, JSONException {
-    String channerUrl = "https://api.twitch.tv/kraken/streams/" + channel;
+	//Checks if a Stream is online
+	public static boolean checkIfOnline(String channel) throws IOException, JSONException {
+		String channerUrl = "https://api.twitch.tv/kraken/streams/" + channel;
 
-    String jsonText = readFromUrl(channerUrl);// reads text from URL
-    JSONObject json = new JSONObject(jsonText);
+		String jsonText = readFromUrl(channerUrl);// reads text from URL
+		JSONObject json = new JSONObject(jsonText);
 
-    return !json.isNull("stream");
-}
+		return !json.isNull("stream");
+	}
 
-//method to help read URL into JSON object
-private static String readFromUrl(String url) throws IOException {
-  URL page = new URL(url);
-  StringBuilder sb = new StringBuilder();
-  Scanner scanner = null;
-  try{
-      scanner = new Scanner(page.openStream(), StandardCharsets.UTF_8.name());
-      while (scanner.hasNextLine()){
-          sb.append(scanner.nextLine());
-      }
-  }finally{
-      if (scanner!=null)
-          scanner.close();
-  }
-  return sb.toString();
-}
+	//method to help read URL into JSON object
+	private static String readFromUrl(String url) throws IOException {
+		URL page = new URL(url);
+		StringBuilder sb = new StringBuilder();
+		Scanner scanner = null;
+		try{
+			scanner = new Scanner(page.openStream(), StandardCharsets.UTF_8.name());
+			while (scanner.hasNextLine()){
+				sb.append(scanner.nextLine());
+			}
+		}finally{
+			if (scanner!=null)
+				scanner.close();
+		}
+		return sb.toString();
+	}
 
-public static void playAudio() throws IOException {
-	URL path = MainInterface.class.getResource("sound1.wav");
-	File soundFile = new File(path.getFile());
-	try {
-		AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-		Clip clip = AudioSystem.getClip();
-		
-		clip.open(audioIn);
-		clip.start();
-	} 
-	catch (UnsupportedAudioFileException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (LineUnavailableException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}	
-}
+	public static void playAudio() throws IOException {
+		URL path = MainInterface.class.getResource(currentDirectory + "\\src\\ambient_twitch_notifs\\sound1.wav");
+		File soundFile = new File(path.getFile());
+		try {
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
 
-public static int getResponseCode(String urlString) throws MalformedURLException, IOException {
-    URL u = new URL(urlString); 
-    HttpURLConnection huc =  (HttpURLConnection)  u.openConnection(); 
-    huc.setRequestMethod("GET"); 
-    huc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
-    huc.connect(); 
-    return huc.getResponseCode();
-}
+			clip.open(audioIn);
+			clip.start();
+		}
+		catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static int getResponseCode(String urlString) throws MalformedURLException, IOException {
+		URL u = new URL(urlString);
+		HttpURLConnection huc =  (HttpURLConnection)  u.openConnection();
+		huc.setRequestMethod("GET");
+		huc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+		huc.connect();
+		return huc.getResponseCode();
+	}
+
+	public static boolean isInteger( String input ) {
+		try {
+			Integer.parseInt( input );
+			return true;
+		}
+		catch( Exception e ) {
+			return false;
+		}
+	}
 
 }
